6b83e902aab6ee757e2c2cd5d6ffba2abcadf0c3
HachemDhawadiSE <hachemdhawadi1@gmail.com>
2024-02-14 20:02:28 +0100

    Création Crud Catégories

--- src/main/java/edu/esprit/services/ServiceCategorie.java
+++ src/main/java/edu/esprit/services/ServiceCategorie.java
@@ -1,4 +1,93 @@
 package edu.esprit.services;
 
-public class ServiceCategorie {
+import edu.esprit.entities.Categorie;
+import edu.esprit.utils.DataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ServiceCategorie implements Iservice<Categorie> {
+    Connection cnx = DataSource.getInstance().getCnx();
+
+    @Override
+    public void ajouter(Categorie categorie) {
+        String req = "INSERT INTO `categorie`(`nom`, `description`) VALUES (?,?)";
+        try (PreparedStatement ps = cnx.prepareStatement(req)) {
+            ps.setString(1, categorie.getNom());
+            ps.setString(2, categorie.getDescription());
+            ps.executeUpdate();
+            System.out.println("Category added!");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void modifier(Categorie categorie) {
+        String req = "UPDATE `categorie` SET `nom` = ?, `description` = ? WHERE `id_categorie` = ?";
+        try (PreparedStatement ps = cnx.prepareStatement(req)) {
+            ps.setString(1, categorie.getNom());
+            ps.setString(2, categorie.getDescription());
+            ps.setInt(3, categorie.getId_categorie());
+            ps.executeUpdate();
+            System.out.println("Category modified!");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void supprimer(int id_categorie) {
+        String req = "DELETE FROM `categorie` WHERE `id_categorie` = ?";
+        try (PreparedStatement ps = cnx.prepareStatement(req)) {
+            ps.setInt(1, id_categorie);
+            ps.executeUpdate();
+            System.out.println("Category deleted!");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public Set<Categorie> getAll() {
+        Set<Categorie> categories = new HashSet<>();
+        String req = "SELECT * FROM `categorie`";
+        try (PreparedStatement ps = cnx.prepareStatement(req);
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                Categorie categorie = new Categorie();
+                categorie.setId_categorie(rs.getInt("id_categorie"));
+                categorie.setNom(rs.getString("nom"));
+                categorie.setDescription(rs.getString("description"));
+                categories.add(categorie);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return categories;
+    }
+
+    @Override
+    public Categorie getOneByID(int id) {
+        String req = "SELECT * FROM `categorie` WHERE `id_categorie` = ?";
+        try (PreparedStatement ps = cnx.prepareStatement(req)) {
+            ps.setInt(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    Categorie categorie = new Categorie();
+                    categorie.setId_categorie(rs.getInt("id_categorie"));
+                    categorie.setNom(rs.getString("nom"));
+                    categorie.setDescription(rs.getString("description"));
+                    return categorie;
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 }
--- src/main/java/edu/esprit/tests/Main.java
+++ src/main/java/edu/esprit/tests/Main.java
@@ -2,6 +2,7 @@ package edu.esprit.tests;
 
 import edu.esprit.entities.Categorie;
 import edu.esprit.entities.Cours;
+import edu.esprit.services.ServiceCategorie;
 import edu.esprit.utils.DataSource;
 
 public class Main {
@@ -9,5 +10,11 @@ public class Main {
         /*DataSource.getInstance(); Test connexion base
         Categorie c = new Categorie(1,"java","java cours");
         System.out.println(c);*/
+
+        /*ServiceCategorie sc = new ServiceCategorie();
+        //sc.ajouter(new Categorie("Web","all belogns to Web"));
+        //sc.modifier(new Categorie(1,"new Mobile",sc.getOneByID(1).getDescription()));
+        //sc.supprimer(1);
+        System.out.println(sc.getOneByID(2));*/
     }
 }
