6dd8300c7b8bd16fd0c0d770174a004fcbb2bd95
HachemDhawadiSE <hachemdhawadi1@gmail.com>
2024-02-14 19:07:04 +0100

    ajouter les dependences et connexion

--- src/main/java/edu/esprit/tests/Main.java
+++ src/main/java/edu/esprit/tests/Main.java
@@ -1,9 +1,10 @@
 package edu.esprit.tests;
 
 import edu.esprit.entities.Cours;
+import edu.esprit.utils.DataSource;
 
 public class Main {
     public static void main(String[] args) {
-
+        //DataSource.getInstance(); Test connexion base
     }
 }
