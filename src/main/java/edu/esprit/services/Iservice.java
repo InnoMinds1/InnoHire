e46ac0a4212466304512f37f1851d39729b3edf4
HachemDhawadiSE <hachemdhawadi1@gmail.com>
2024-02-14 19:50:46 +0100

    Création Interface Service

--- src/main/java/edu/esprit/services/Iservice.java
+++ src/main/java/edu/esprit/services/Iservice.java
@@ -1,4 +1,12 @@
 package edu.esprit.services;
 
-public interface Iservice {
+import java.util.Set;
+
+public interface Iservice <T>{
+    public void ajouter(T t);
+    public void modifier(T t);
+    public void supprimer(int id);
+    public Set<T> getAll();
+    public T getOneByID(int id);
+
 }
--- src/main/java/edu/esprit/services/ServiceCours.java
+++ src/main/java/edu/esprit/services/ServiceCours.java
@@ -1,4 +1,3 @@
 package edu.esprit.services;
-
 public class ServiceCours {
 }
