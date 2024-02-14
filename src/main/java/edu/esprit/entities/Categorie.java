50fd9a92da783ca0787588b2fd1c63cbd7496c56
HachemDhawadiSE <hachemdhawadi1@gmail.com>
2024-02-14 19:24:39 +0100

    Création classe Catégorie

--- src/main/java/edu/esprit/entities/Categorie.java
+++ src/main/java/edu/esprit/entities/Categorie.java
@@ -1,4 +1,80 @@
 package edu.esprit.entities;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Categorie {
+    private int id_categorie;
+    private String nom,description;
+
+    private List<Cours> listCours;
+
+    public List<Cours> getListCours() {
+        return listCours;
+    }
+
+    public void setListCours(List<Cours> listCours) {
+        this.listCours = listCours;
+    }
+
+    public Categorie() {
+    }
+
+    public Categorie(String nom, String description) {
+        this.nom = nom;
+        this.description = description;
+    }
+
+    public Categorie(int id_categorie, String nom, String description) {
+        this.id_categorie = id_categorie;
+        this.nom = nom;
+        this.description = description;
+    }
+
+    public int getId_categorie() {
+        return id_categorie;
+    }
+
+    public void setId_categorie(int id_categorie) {
+        this.id_categorie = id_categorie;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Categorie{" +
+                "nom='" + nom + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categorie categorie = (Categorie) o;
+        return id_categorie == categorie.id_categorie;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id_categorie);
+    }
 }
--- src/main/java/edu/esprit/tests/Main.java
+++ src/main/java/edu/esprit/tests/Main.java
@@ -1,10 +1,13 @@
 package edu.esprit.tests;
 
+import edu.esprit.entities.Categorie;
 import edu.esprit.entities.Cours;
 import edu.esprit.utils.DataSource;
 
 public class Main {
     public static void main(String[] args) {
-        //DataSource.getInstance(); Test connexion base
+        /*DataSource.getInstance(); Test connexion base
+        Categorie c = new Categorie(1,"java","java cours");
+        System.out.println(c);*/
     }
 }
