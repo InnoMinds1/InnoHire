c245ed6e9bf7d1905ca79f4b688f0c219dfa3df8
youssef sayari <youssef.sayari@esprit.tn>
2024-02-15 15:07:09 +0100

    crude_tableEtablissment_complet

--- projet_pidev/src/main/java/edu/esprit/tests/Main.java
+++ projet_pidev/src/main/java/edu/esprit/tests/Main.java
@@ -1,36 +1,44 @@
 package edu.esprit.tests;
 
-import edu.esprit.entities.Admin;
-import edu.esprit.entities.Candidat;
-import edu.esprit.entities.Representant;
-import edu.esprit.services.ServiceUtilisateur;
-import edu.esprit.utils.DataSource;
+import edu.esprit.entities.Etablissement;
+import edu.esprit.services.ServiceEtablissement;
 
 public class Main {
     public static void main(String[] args)
     {
-        DataSource.getInstance();
-        ServiceUtilisateur sr= new ServiceUtilisateur();
-        Admin a = new Admin(1,234,"AMENALLAH123","KTHIRI","amenallah@esprit.tn","ELKING");
-        //sr.ajouter(a);
-        Representant r = new Representant(2,33,"AMENALLAH","KTHIRI","amenallah@esprit.tn","ELKING");
-       sr.ajouter(r);
-        Candidat c= new Candidat(3,322,"YO222","ee","SLFJ@","TRAHHH");
-        //sr.ajouter(c);
-
-        System.out.println(sr.getAll());
-        System.out.println(sr.getAll_admin());
-        sr.modifier(c);
-        sr.modifier_admin(a);
-        sr.supprimer(44);
-        sr.supprimer(3);
-        sr.supprimer_admin(44);
-        sr.supprimer_admin(12);
-        System.out.println(sr.getOneByID(1));
-        System.out.println(sr.getOneAdminByID(12));
-        System.out.println(sr.getOneAdminByID(1));
 
 
+/*----------------------------------------------------Sayari---------------------------------------*/
+
+        /*
+        DataSource ds = new DataSource();
+        DataSource ds1 = new DataSource();
+        DataSource ds2 = new DataSource();
+        DataSource ds3 = new DataSource();
+       --> bchaamlk barcha instance w twli bel zhar donc lezem patron de conception singleton
+         */
+
+        ServiceEtablissement se = new ServiceEtablissement();
+
+        /*----------------ajouter-------------------------*/
+
+        //se.ajouter(new Etablissement("isie","Ghazela",123,"Faculte",2));
+
+        /*----------------modifier-------------------------*/
+
+        Etablissement etablissementModifie = new Etablissement(1,"test","Ghazela",123,"Faculte",1);
+         //se.modifier(etablissementModifie);
+
+        /*-------------------Supprimer-------------------*/
+        //se.supprimer(1);
+
+        /*-------------------affichage_par_ID-------------------*/
+        //System.out.println(se.getOneByID(2));
+
+        /*----------------afficher tout-------------------------*/
+        System.out.println(se.getAll());
+
+        /*--------------------------------------End_Sayari----------------------------------------------------*/
 
 
     }
