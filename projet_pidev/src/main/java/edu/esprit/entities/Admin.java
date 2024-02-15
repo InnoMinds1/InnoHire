de387585b48a67d7fac437b9b1a3c0e650313584
amenk21 <amenallah.kthiri@esprit.tn>
2024-02-15 11:14:35 +0100

    .

---
+++ projet_pidev/src/main/java/edu/esprit/entities/Admin.java
@@ -0,0 +1,37 @@
+package edu.esprit.entities;
+
+import java.util.Objects;
+
+public class Admin extends Utilisateur{
+
+    public Admin() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+    public String toString() {
+        return "Admin{" +
+                "id_utilisateur=" + this.getId_utilisateur() +
+                ", cin=" + this.getCin() +
+                ", nom='" + this.getNom() + '\'' +
+                ", prenom='" + this.getPrenom() + '\'' +
+                ", adresse='" + this.getAdresse() + '\'' +
+                ", mdp='" + this.getMdp() + '\'' +
+
+                '}';
+    }
+
+    public Admin(int id_utilisateur,int cin,String nom ,String prenom,String adresse, String mdp)
+    {
+        super(id_utilisateur, cin, nom, prenom, adresse, mdp);
+
+    }
+
+}
