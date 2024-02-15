649ad35b48813801419533ec8762f79eb697daf0
youssef sayari <youssef.sayari@esprit.tn>
2024-02-15 16:34:05 +0100

    completed_Crude_table_Wallet

--- projet_pidev/src/main/java/edu/esprit/tests/Main.java
+++ projet_pidev/src/main/java/edu/esprit/tests/Main.java
@@ -1,7 +1,9 @@
 package edu.esprit.tests;
 
 import edu.esprit.entities.Etablissement;
+import edu.esprit.entities.Wallet;
 import edu.esprit.services.ServiceEtablissement;
+import edu.esprit.services.ServiceWallet;
 
 public class Main {
     public static void main(String[] args)
@@ -9,6 +11,8 @@ public class Main {
 
 
 /*----------------------------------------------------Sayari---------------------------------------*/
+
+
 
         /*
         DataSource ds = new DataSource();
@@ -36,9 +40,39 @@ public class Main {
         //System.out.println(se.getOneByID(2));
 
         /*----------------afficher tout-------------------------*/
-        System.out.println(se.getAll());
+        //System.out.println(se.getAll());
+
+
 
         /*--------------------------------------End_Sayari----------------------------------------------------*/
+
+                /*
+        DataSource ds = new DataSource();
+        DataSource ds1 = new DataSource();
+        DataSource ds2 = new DataSource();
+        DataSource ds3 = new DataSource();
+       --> bchaamlk barcha instance w twli bel zhar donc lezem patron de conception singleton
+         */
+
+        ServiceWallet sw = new ServiceWallet();
+
+        /*----------------ajouter-------------------------*/
+
+        //sw.ajouter(new Wallet(100,7));
+
+        /*----------------modifier-------------------------*/
+
+        Wallet walletModifie = new Wallet(3,200,7);
+       // sw.modifier(walletModifie);
+
+        /*-------------------Supprimer-------------------*/
+       // sw.supprimer(4);
+
+        /*-------------------affichage_par_ID-------------------*/
+        System.out.println(sw.getOneByID(3));
+
+        /*----------------afficher tout-------------------------*/
+       // System.out.println(sw.getAll());
 
 
     }
