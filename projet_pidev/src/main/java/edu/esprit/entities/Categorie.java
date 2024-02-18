4e3ef534afa9692f29161f5e60dad7232104979f
HachemDhawadiSE <hachemdhawadi1@gmail.com>
2024-02-18 19:51:58 +0100

    Class Catégorie

---
+++ projet_pidev/src/main/java/edu/esprit/entities/Categorie.java
@@ -0,0 +1,70 @@
+package edu.esprit.entities;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Categorie {
+    private int id_categorie;
+    private String nom,description;
+    
+    public Categorie() {
+    }
+
+    public Categorie(String nom, String description) {
+        this.nom = nom;
+        this.description = description;
+    }
+
+    public Categorie(int id_categorie, String nom, String description) {
+        this.id_categorie = id_categorie;
+        this.nom = nom;
+        this.description = description;
+    }
+
+    public int getId_categorie() {
+        return id_categorie;
+    }
+
+    public void setId_categorie(int id_categorie) {
+        this.id_categorie = id_categorie;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Categorie{" +
+                "nom='" + nom + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categorie categorie = (Categorie) o;
+        return id_categorie == categorie.id_categorie;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id_categorie);
+    }
+}
