9ab315efbe64a128ddd97fa3bc5d56e4d3c7bdf4
HachemDhawadiSE <hachemdhawadi1@gmail.com>
2024-02-18 19:45:21 +0100

    architecture projet

--- projet_pidev/src/main/java/edu/esprit/tests/Main.java
+++ projet_pidev/src/main/java/edu/esprit/tests/Main.java
@@ -1,37 +1,8 @@
 package edu.esprit.tests;
 
-import edu.esprit.entities.Admin;
-import edu.esprit.entities.Candidat;
-import edu.esprit.entities.Representant;
-import edu.esprit.services.ServiceUtilisateur;
-import edu.esprit.utils.DataSource;
-
 public class Main {
     public static void main(String[] args)
     {
-        DataSource.getInstance();
-        ServiceUtilisateur sr= new ServiceUtilisateur();
-        Admin a = new Admin(1,234,"AMENALLAH123","KTHIRI","amenallah@esprit.tn","ELKING");
-        //sr.ajouter(a);
-        Representant r = new Representant(2,33,"AMENALLAH","KTHIRI","amenallah@esprit.tn","ELKING");
-       sr.ajouter(r);
-        Candidat c= new Candidat(3,322,"YO222","ee","SLFJ@","TRAHHH");
-        //sr.ajouter(c);
-
-        System.out.println(sr.getAll());
-        System.out.println(sr.getAll_admin());
-        sr.modifier(c);
-        sr.modifier_admin(a);
-        sr.supprimer(44);
-        sr.supprimer(3);
-        sr.supprimer_admin(44);
-        sr.supprimer_admin(12);
-        System.out.println(sr.getOneByID(1));
-        System.out.println(sr.getOneAdminByID(12));
-        System.out.println(sr.getOneAdminByID(1));
-
-
-
 
     }
 }
