80faabf48a2e9187d6dd52099b328b3dd84f71c9
HachemDhawadiSE <hachemdhawadi1@gmail.com>
2024-02-19 22:52:42 +0100

    Creation ServiceMessagerie

---
+++ projet_pidev/src/main/java/edu/esprit/services/ServiceMessagerie.java
@@ -0,0 +1,32 @@
+package edu.esprit.services;
+
+import edu.esprit.entities.Messagerie;
+
+import java.util.Set;
+
+public class ServiceMessagerie implements Iservice<Messagerie> {
+    @Override
+    public void ajouter(Messagerie messagerie) {
+        
+    }
+
+    @Override
+    public void modifier(Messagerie messagerie) {
+
+    }
+
+    @Override
+    public void supprimer(int id) {
+
+    }
+
+    @Override
+    public Set<Messagerie> getAll() {
+        return null;
+    }
+
+    @Override
+    public Messagerie getOneByID(int id) {
+        return null;
+    }
+}
