2d31c8fdf5d6db435b5b84ca1cc24d0e863968f2
HachemDhawadiSE <hachemdhawadi1@gmail.com>
2024-02-19 20:26:54 +0100

    Creation Service Reclamation

---
+++ projet_pidev/src/main/java/edu/esprit/services/ServiceReclamation.java
@@ -0,0 +1,32 @@
+package edu.esprit.services;
+
+import edu.esprit.entities.Reclamation;
+
+import java.util.Set;
+
+public class ServiceReclamation implements Iservice<Reclamation>{
+    @Override
+    public void ajouter(Reclamation reclamation) {
+
+    }
+
+    @Override
+    public void modifier(Reclamation reclamation) {
+
+    }
+
+    @Override
+    public void supprimer(int id) {
+
+    }
+
+    @Override
+    public Set<Reclamation> getAll() {
+        return null;
+    }
+
+    @Override
+    public Reclamation getOneByID(int id) {
+        return null;
+    }
+}
