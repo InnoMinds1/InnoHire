24f6bb197e4ee3c2518dcdf12f6797be7f2cd256
youssef sayari <youssef.sayari@esprit.tn>
2024-02-20 20:13:42 +0100

    base_afficher_avec_css added

--- projet_pidev/src/main/java/edu/esprit/controllers/AfficherEtablissement.java
+++ projet_pidev/src/main/java/edu/esprit/controllers/AfficherEtablissement.java
@@ -1,5 +1,6 @@
 package edu.esprit.controllers;
 
+import edu.esprit.entities.Etablissement;
 import edu.esprit.services.ServiceEtablissement;
 
 import javafx.fxml.FXML;
@@ -8,7 +9,9 @@ import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 public class AfficherEtablissement implements Initializable {
     @FXML
@@ -19,7 +22,16 @@ public class AfficherEtablissement implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
-            LLetablissements.setText(se.getAll().toString());
+            Set<Etablissement> etablissementsList = se.getAll();
+
+            // Construire la chaîne avec chaque établissement sur une nouvelle ligne
+            StringBuilder etablissementsStringBuilder = new StringBuilder();
+            for (Etablissement etablissement : etablissementsList) {
+                etablissementsStringBuilder.append(etablissement).append("\n");
+            }
+
+            // Afficher la chaîne résultante dans votre composant (LLetablissements)
+            LLetablissements.setText(etablissementsStringBuilder.toString());
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
