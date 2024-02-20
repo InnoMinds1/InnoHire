79f96005581dc4d5283016bb87744f0d37a59063
HachemDhawadiSE <hachemdhawadi1@gmail.com>
2024-02-20 15:57:50 +0100

    Function ajouter ServiceMessagerie

--- projet_pidev/src/main/java/edu/esprit/services/ServiceMessagerie.java
+++ projet_pidev/src/main/java/edu/esprit/services/ServiceMessagerie.java
@@ -1,13 +1,29 @@
 package edu.esprit.services;
 
 import edu.esprit.entities.Messagerie;
+import edu.esprit.utils.DataSource;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Set;
 
 public class ServiceMessagerie implements Iservice<Messagerie> {
+    private Connection cnx = DataSource.getInstance().getCnx();
     @Override
-    public void ajouter(Messagerie messagerie) {
-        
+    public void ajouter(Messagerie mess) {
+        String req = "INSERT INTO `messagerie`(`date`, `type`, `contenu`, `sender_id`, `reciver_id`) VALUES (?, ?, ?, ?, ?)";
+        try (PreparedStatement ps = cnx.prepareStatement(req)) {
+            ps.setTimestamp(1, new java.sql.Timestamp(mess.getDate().getTime()));
+            ps.setString(2, mess.getType());
+            ps.setString(3, mess.getContenu());
+            ps.setInt(4, mess.getSender_id().getId_utilisateur());
+            ps.setInt(5, mess.getReciver_id().getId_utilisateur());
+            ps.executeUpdate();
+            System.out.println("Messaagerie added!");
+        } catch (SQLException e) {
+            System.err.println("Error adding Messaagerie: " + e.getMessage());
+        }
     }
 
     @Override
--- projet_pidev/src/main/java/edu/esprit/tests/Main.java
+++ projet_pidev/src/main/java/edu/esprit/tests/Main.java
@@ -1,9 +1,11 @@
 package edu.esprit.tests;
 
 
+import edu.esprit.entities.Messagerie;
 import edu.esprit.entities.Publication;
 import edu.esprit.entities.Reclamation;
 import edu.esprit.entities.Utilisateur;
+import edu.esprit.services.ServiceMessagerie;
 import edu.esprit.services.ServiceReclamation;
 import edu.esprit.utils.DataSource;
 
@@ -15,8 +17,11 @@ public class Main {
         DataSource.getInstance();
         //(int id_utilisateur, int cin, String nom , String prenom, String adresse, String mdp)
         Utilisateur user=new Utilisateur(1,11417264,"dhawadi","hachem","bizerte","123456789");
+        Utilisateur user2=new Utilisateur(9,11417264,"dhawadi","hachem","bizerte","123456789");
         Publication pub=new Publication(1,1,5,"mhaf","#mhaf","seen","mhaf.png","19/02/2024");
         ServiceReclamation sr=new ServiceReclamation();
+        ServiceMessagerie sm=new ServiceMessagerie();
+        sm.ajouter(new Messagerie("text","www gafsi",new Date(),user,user2));
         //int status, String type, String titre, String description, String image, Date date, Publication pub, Utilisateur user
         //sr.ajouter(new Reclamation(0,"spam","bleda mhafa","description","mhaf.png",new Date(),pub,user));
         //sr.modifier(new Reclamation(1,1,"spam","modification mhafa","modification","mhaf.png",new Date(),pub,user));
@@ -24,7 +29,7 @@ public class Main {
         //sc.ajouter(new Categorie("Web","all belogns to Web"));
         //sc.modifier(new Categorie(1,"new Mobile",sc.getOneByID(1).getDescription()));
        // sr.supprimer(2);
-        System.out.println(sr.getOneByID(1));
+        //System.out.println(sr.getOneByID(1));
         //System.out.println(sc.getOneByID(2));*/
 
     }
