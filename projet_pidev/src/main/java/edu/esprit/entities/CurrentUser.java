7838b379550a63d4f1428497267086d5a983965a
oumaymadhif <101249813+oumaymadhif@users.noreply.github.com>
2024-03-01 00:03:05 +0100

    design

--- projet_pidev/src/main/java/edu/esprit/entities/CurrentUser.java
+++ projet_pidev/src/main/java/edu/esprit/entities/CurrentUser.java
@@ -1,9 +1,9 @@
 package edu.esprit.entities;
 
 public class CurrentUser {
-    private static int id_utilisateur =17 ;
-    private static  int cin=233;
-    private static String name="AMENN";
+    private static int id_utilisateur =30;
+    private static  int cin=99;
+    private static String name="User";
     private static String prenom;
     private static String adresse;
     private static String mdp;
