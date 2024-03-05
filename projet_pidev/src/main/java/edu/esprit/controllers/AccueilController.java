4142c54f39453b327944f1ff2900f2c7f6352a75
amenk21 <amenallah.kthiri@esprit.tn>
2024-03-05 20:32:56 +0100

    acc

--- projet_pidev/src/main/java/edu/esprit/controllers/AccueilController.java
+++ projet_pidev/src/main/java/edu/esprit/controllers/AccueilController.java
@@ -11,6 +11,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.util.Callback;
 
 import java.io.IOException;
@@ -21,12 +22,56 @@ import java.util.ResourceBundle;
 import java.util.Set;
 
 public class AccueilController implements Initializable {
+
+    @FXML
+    private AnchorPane AnchoPaneMessage131;
+
+    @FXML
+    private Label CurrentUserEmail;
+
     @FXML
     private TextField TFcurrentusercin;
+
+    @FXML
+    private AnchorPane container;
+
+    @FXML
+    private Label currentUserName;
+
+
+
+    @FXML
+    void NavigateToEtablissement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void NavigatoToListUsers(ActionEvent event) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/listUsers.fxml"));
+            TFcurrentusercin.getScene().setRoot(root);
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Sorry");
+            alert.setTitle("Error");
+            alert.show();
+        }
+
+    }
+
+    @FXML
+    void navigatotoAccueil(ActionEvent event) {
+
+    }
+
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         TFcurrentusercin.setText(CurrentUser.getNom());
+        currentUserName.setText(CurrentUser.getNom());
+        CurrentUserEmail.setText(CurrentUser.getAdresse());
 
     }
     @FXML
